dae249f81e6a78ae24cff3b054aa3ad55675855c
gato365 <immanuelwilliams@gato365.com>
2023-04-24 19:44:29 -0700

    Created Server as well

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+// const colors = require('colors');
+// const dotenv = require('dotenv').config();
+// const {errorHandler} = require('./middleware/errorMiddleware');
+const connectDB = require('./config/db');
+const port = process.env.PORT || 5000;
+
+connectDB();
+
+const app = express();
+// Location Matters Here
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+
+app.listen(port, () => console.log(`Server started on port ${port}`)); 
