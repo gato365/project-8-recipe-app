c4cb6de0940d5d79149a04f134307029a462e822
gato365 <immanuelwilliams@gato365.com>
2023-04-24 19:31:23 -0700

    Specify Index  for model

---
+++ server/models/index.js
@@ -0,0 +1,7 @@
+// Bring in Relevant Models
+const User = require('./user');
+const Recipe = require('./recipe');
+const Cuisine = require('./cuisine');
+
+// Export Models
+module.exports = { User, Recipe, Cuisine };
