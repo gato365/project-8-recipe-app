3ab4613c2f387e551853ca15f10c044872f0bcb0
gato365 <immanuelwilliams@gato365.com>
2023-04-24 19:18:11 -0700

    Develop cuisine model and other files

---
+++ server/models/Cuisine.js
@@ -0,0 +1,23 @@
+// Create a mongoose model for a cuisine
+// 1. Cuisine:
+// - id: A unique identifier for each cuisine type.
+// - name: The name of the cuisine type (e.g., Italian, Mexican, Indian, etc.).
+// - recipes: A list of recipes associated with the cuisine type.
+
+
+const { Schema, model } = require('mongoose');
+
+const cuisineSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    recipes: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Recipe'
+        }
+    ]
+});
