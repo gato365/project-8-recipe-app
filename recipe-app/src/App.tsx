4cc46daec5de6042a502591f824d16770c49b92c
gato365 <immanuelwilliams@gato365.com>
2023-04-29 19:25:06 -0700

    Created the capacity to make app run

--- recipe-app/src/App.tsx
+++ recipe-app/src/App.tsx
@@ -10,6 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        Learning to make things happen
         <a
           className="App-link"
           href="https://reactjs.org"
