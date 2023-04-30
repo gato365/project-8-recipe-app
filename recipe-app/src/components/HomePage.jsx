1e5166590b37a076e28b2dd42603d1f7f28343ec
gato365 <immanuelwilliams@gato365.com>
2023-04-29 20:08:24 -0700

    Made changes to homepage css

--- recipe-app/src/components/HomePage.jsx
+++ recipe-app/src/components/HomePage.jsx
@@ -2,29 +2,36 @@ import React from 'react';
 import Image from './Image';
 import Section from './Section';
 import Article from './Article';
+import './HomePage.css';
 
 function HomePage() {
   return (
     <div>
       <Image src="recipe.jpg" alt="Recipe Image" />
 
-      <Section title="Top Stories">
-        <Article image="story1.jpg" title="Story Title 1" content="Story content goes here." />
-        <Article title="Story Title 2" content="Story content goes here." image="story2.jpg" />
-        <Article image="story3.jpg" title="Story Title 3" content="Story content goes here." />
-      </Section>
+      <div className="section-container">
+        <Section title="Top Stories">
+          <Article image="story1.jpg" title="Story Title 1" content="Story content goes here." />
+          <Article title="Story Title 2" content="Story content goes here." image="story2.jpg" />
+          <Article image="story3.jpg" title="Story Title 3" content="Story content goes here." />
+        </Section>
 
-      <Section title="Stories">
-        <Article title="Story Title 4" content="Story content goes here." image="story4.jpg" />
-        <Article image="story5.jpg" title="Story Title 5" content="Story content goes here." />
-        <Article title="Story Title 6" content="Story content goes here." image="story6.jpg" />
-      </Section>
+        <div className="vertical-line"></div>
 
-      <Section title="Features">
-        <Article image="feature1.jpg" title="Feature Title 1" content="Feature content goes here." />
-        <Article title="Feature Title 2" content="Feature content goes here." image="feature2.jpg" />
-        <Article image="feature3.jpg" title="Feature Title 3" content="Feature content goes here." />
-      </Section>
+        <Section title="Stories">
+          <Article title="Story Title 4" content="Story content goes here." image="story4.jpg" />
+          <Article image="story5.jpg" title="Story Title 5" content="Story content goes here." />
+          <Article title="Story Title 6" content="Story content goes here." image="story6.jpg" />
+        </Section>
+
+        <div className="vertical-line"></div>
+
+        <Section title="Features">
+          <Article image="feature1.jpg" title="Feature Title 1" content="Feature content goes here." />
+          <Article title="Feature Title 2" content="Feature content goes here." image="feature2.jpg" />
+          <Article image="feature3.jpg" title="Feature Title 3" content="Feature content goes here." />
+        </Section>
+      </div>
     </div>
   );
 }
