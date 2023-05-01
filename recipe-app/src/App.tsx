7960e24529a22ff11c942fe7b93c7f059849be92
gato365 <immanuelwilliams@gato365.com>
2023-05-01 10:55:08 -0700

    Made App appear again by fixing header info

--- recipe-app/src/App.tsx
+++ recipe-app/src/App.tsx
@@ -3,14 +3,32 @@ import logo from './logo.svg';
 import './App.css';
 import HomePage from './components/HomePage';
 import NavBar from './components/NavBar';
+import { BrowserRouter as Router, Route,Routes } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Header from './components/Header';
 
 function App() {
   return (
 
-    <div className="App">
-      <NavBar />
-      <HomePage />
+    <div>
+         <Router>
+     <div className="container">
+          <Header />
+          <h1>App</h1>
+          {/* <Routes>
+            <Route path='/' element={<Dashboard />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/register' element={<Register />} />
+          </Routes> */}
+          <HomePage />
+        </div>
+        </Router> 
+        <ToastContainer />
     </div>
+   
+
+
   );
 }
 
